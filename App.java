1d8f65786a8f535a6c0e7f499003cc7ed0120bdb
Chrisnald Xyronn A.Bartolome <111546428+s4intsai@users.noreply.github.com>
2022-09-06 02:43:22 +0100

    Create App.java

---
+++ App.java
@@ -0,0 +1,21 @@
+public class App {
+   public static void main (String[] args ) {
+
+    // Character class
+    student me = new student();
+
+    me.Surname = "Bartolome";
+    me.firstname = "Chrisnald";
+    me.middleinital = 'A';
+    me.dateOfBirth = "08/08/03";
+    me.studentnumber = 202210446;
+    me.studentEmailAddress = "bartolomeca@students.natioal.edu.ph";
+    me.IamAwesome = true;
+    me.sayMyStudentNumber();
+    me.sayMyEmailAdress();
+    me.sayIAwesome();
+
+
+
+    }
+}
