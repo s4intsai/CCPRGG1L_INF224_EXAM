aad3b0cdab30c4590731d42fd5225075b40be18c
Chrisnald Xyronn A.Bartolome <111546428+s4intsai@users.noreply.github.com>
2022-09-06 02:44:30 +0100

    Create student.java

---
+++ student.java
@@ -0,0 +1,28 @@
+public class student {
+    String Surname;
+    String firstname;
+    char middleinital;
+    String dateOfBirth;
+    int studentnumber;
+    String studentEmailAddress;
+    boolean IamAwesome;
+    
+
+public void sayMyStudentNumber() {
+    System.out.println("My Student Number is " + studentnumber);
+}
+
+public void sayMyEmailAdress() {
+    System.out.println("My Email is " + studentEmailAddress);
+}
+public void  sayIAwesome() {
+    System.out.println("Am I awsome");
+    
+
+
+
+
+}
+
+    
+}
